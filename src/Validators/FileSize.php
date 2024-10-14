64e08b51dfb24b51f05df54de74c48b642273efc
changwoo <cs.chwnam@gmail.com>
2024-10-14 15:06:27 +0900

    임시 코드 커밋

--- src/Validators/FileSize.php
+++ src/Validators/FileSize.php
@@ -2,7 +2,64 @@
 
 namespace ShoplicKr\Uploady\Validators;
 
+use ShoplicKr\Uploady\File;
+
 class FileSize implements Validator
 {
+    private int $exactSize;
+
+    private int $maxSize;
+
+    private int $minSize;
+
+    public function __construct(array $args = [])
+    {
+        $args = wp_parse_arge(
+            $args,
+            [
+                'exact' => '',
+                'max'   => '',
+                'min'   => '',
+            ],
+        );
+
+        $this->exactSize = self::parseSize($args['exact']);
+        $this->maxSize   = self::parseSize($args['max']);
+        $this->minSize   = self::parseSize($args['min']);
+    }
+
+    public function validate(File $item)
+    {
+        if ($this->exactSize > 0 && $this->exactSize !== $item->size) {
+            // throw.
+        }
+
+        if ($this->minSize > 0 && $this->minSize > $item->size) {
+            // throw.
+        }
+
+        if ($this->maxSize > 0 && $this->maxSize < $this->size) {
+            // throw.
+        }
+    }
+
+    private static function parseSize(string $size): int
+    {
+        $output = -1;
+
+        if (preg_match('/^([0-9]*\.?[0-9]+)([KMGT]?)$/i', $size, $matches)) {
+            $amount = (float)$matches[1];
+            $unit   = strtoupper($matches[2]);
+            $output = (int)match ($unit) {
+                'K' => $amount * KB_IN_BYTES,
+                'M' => $amount * MB_IN_BYTES,
+                'G' => $amount * GB_IN_BYTES,
+                'T' => $amount * TB_IN_BYTES,
+                default => $amount,
+            };
+        }
+
+        return $output ?: -1;
+    }
 }
 
--- src/Validators/Validator.php
+++ src/Validators/Validator.php
@@ -2,6 +2,9 @@
 
 namespace ShoplicKr\Uploady\Validators;
 
+use ShoplicKr\Uploady\File;
+
 interface Validator
 {
+    public function validate(File $item);
 }
